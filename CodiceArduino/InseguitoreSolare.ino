502999aafb275e485cdb00379a8d151aac84f113
Marco0126 <65389757+Marco0126@users.noreply.github.com>
2020-11-22 21:13:14 +0100

    Update InseguitoreSolare.ino

--- CodiceArduino/InseguitoreSolare.ino
+++ CodiceArduino/InseguitoreSolare.ino
@@ -1,9 +1,33 @@
+#include <Servo.h>
+ 
+int sensorPin = A0;
+int servoPin  = 9;
+ 
+int sensorValue = 0;
+int servoGrad = 90;
+int tolleranza = 40;
+ 
+Servo myservo;
+ 
 void setup() {
-  // put your setup code here, to run once:
-
+  pinMode( sensorPin, INPUT);
+  myservo.attach( servoPin );
+  myservo.write( servoGrad );
 }
-
+ 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  sensorValue = analogRead(sensorPin);
+  if ( sensorValue < (512-tolleranza) )
+  {
+    if (servoGrad < 180) servoGrad++;
+  }
+ 
+  if ( sensorValue > (512+tolleranza) )
+  {
+    if (servoGrad > 0) servoGrad--;
+  }
+ 
+  myservo.write( servoGrad ); 
+ 
+  delay(100);
 }
