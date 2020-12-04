346f52ea8084bf4ceee0130db1f04e5ff6828869
andredecah <andredecaad@gmail.com>
2020-12-04 22:21:53 +0100

    Commit prima versione del codice finito

---
+++ PROGETTO FINALE/CodiceFinale_V1.ino
@@ -0,0 +1,62 @@
+#include <Stepper.h> //Libreria per motorino
+
+Stepper motorino(2048, 11, 10, 9, 8); //Creo il motorino
+
+int fotosensoreSX = A0; //fotoresistenza sinistra
+int fotosensoreDX = A1; //fotoresistenza destra
+
+int luminositaSX; //Il valore letto dalla fotoresistenza sinistra
+int luminositaDX; //Il valore letto dalla fotoresistenza destra
+ 
+void setup() {
+  
+  Serial.begin(9600);
+  motorino.setSpeed(10); //Velocità usata dal motorino
+  pinMode(fotosensoreSX, INPUT); //La fotoresistenza legge
+  pinMode(fotosensoreDX, INPUT); //La fotoresistenza legge
+  
+}
+ 
+void loop() {
+
+  float gradi = 90; //Creo una variabile per rimappare i gradi da 2048 ai 360
+  gradi = map(gradi, 0, 360, 0, 2048); //Li rimappo
+  
+  luminositaSX = analogRead(fotosensoreSX); //Lettura della luminosità della fotoresistenza di sinistra
+  lumonisitaDX = analogRead(fotosensoreDX); //Lettura della luminosità della fotoresistenza di destra
+
+  //Se il fotosensore di sinistra rileva più luce di quella di destra
+  if (luminositaSX > luminositaDX) 
+  {
+    //Il motorino gira verso sinistra
+    Sinistra();
+  }
+
+  //Se il fotosensore di sinistra rileva più luce di quella di destra
+  else if (luminositaDX > luminositaSX)
+  {
+    //Il motorino gira verso destra
+    Destra();
+  }
+  
+} 
+
+//Metodo sinistra
+void Sinistra()
+{
+  //Rimetto i gradi anche nella funzione
+  float gradi = 90;
+  gradi = map(gradi, 0, 360, 0, 2048);
+  //Giro verso sinistra di 90 gradi
+  myStepper.step(-gradi);
+}
+
+
+void Destra()
+{
+  //Rimetto i gradi anche nella funzione
+  float gradi = 90;
+  gradi = map(gradi, 0, 360, 0, 2048);
+  //Giro verso destra di 90 gradi
+  myStepper.step(gradi);
+}
